27db9ea026c855f001134aa53fff799cb716e35d
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-22 09:48:03 +0100

    Kommentare

--- book-rating/src/app/books/shared/book.ts
+++ book-rating/src/app/books/shared/book.ts
@@ -6,3 +6,8 @@ export interface Book {
   price: number;
   rating: number;
 }
+
+/* Warum Interfaces+Rohdaten statt Klassen?
+- 1. JSON-(De)Serialisierung
+- 2. Immutability: Klonen geht mit Plain Objects einfacher
+*/
