d0e61c7f43ed64a874f0e223dd5f30174711b071
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-23 14:51:17 +0100

    Routing: Weiterleitung und Wildcard-Route

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { booksRoutes } from './books/books.routes';
+import { ErrorPage } from './error-page/error-page';
 
 export const routes: Routes = [
-  ...booksRoutes
+  // bei Weiterleitung vom leeren Pfad ist (fast) immer pathMatch:full nötig
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  ...booksRoutes,
+  { path: '**', component: ErrorPage },
 ];
