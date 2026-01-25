05fb8e5222ea607fa5f118d74baab67c918d11f7
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-25 15:22:01 +0100

    RxJS-Playground hinzugefügt

---
+++ rxjs-playground/src/app/exercises.ts
@@ -0,0 +1,15 @@
+export interface ExerciseEntry {
+  name: string;
+  slug: string;
+}
+
+export const exercisesList: ExerciseEntry[] = [
+  { name: 'Creating Observables', slug: 'creating' },
+  { name: 'Window resize: Observables from events', slug: 'fromevent' },
+  { name: 'Multicasting with Subjects', slug: 'multicast' },
+  { name: 'Error handling', slug: 'errorhandling' },
+  { name: 'How to unsubscribe and avoid memory leaks', slug: 'unsubscribe' },
+  { name: 'Higher Order Observables with concatMap, mergeMap, switchMap, exhaustMap', slug: 'higherorder' },
+  { name: '[EXTRA] Chat: Merging Observables', slug: 'chat' },
+  { name: '[EXTRA] Drag and drop', slug: 'dragdrop' },
+];
