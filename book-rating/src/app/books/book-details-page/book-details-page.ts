9481f2560afc0e3056ebdc756e6eb54f03c509ea
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-26 11:02:21 +0100

    Routenparameter abrufen

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-details-page',
@@ -8,5 +8,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './book-details-page.scss',
 })
 export class BookDetailsPage {
+  #route = inject(ActivatedRoute);
+
+  constructor() {
+    // PULL
+    // const isbn = this.#route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+
+    // PUSH
+    this.#route.paramMap.subscribe(params => {
+      // Im Zweifel bitte if-Abfrage oder Fallback verwenden
+      const isbn = params.get('isbn')!; // Non-Null Assertion, bitte vorsichtig verwenden!
+      console.log(isbn);
+    });
+  }
 
 }
+
+
+/*
+TODO Detailseite
+- BookStore HTTP: einzelnes Buch abrufen
+- Buch anzeigen
+
+*/
