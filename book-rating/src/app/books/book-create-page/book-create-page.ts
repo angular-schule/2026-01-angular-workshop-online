c2ca95ba2e20bdaa799233bbaf94e48fabcc0f14
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-26 11:48:15 +0100

    Vorbereitung Routen für Formulare

---
+++ book-rating/src/app/books/book-create-page/book-create-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-create-page',
+  imports: [],
+  templateUrl: './book-create-page.html',
+  styleUrl: './book-create-page.scss',
+})
+export class BookCreatePage {
+
+}
