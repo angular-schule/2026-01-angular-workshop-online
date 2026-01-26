29e27edab546b6227b728f058393da84081e39d6
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-26 12:28:30 +0100

    Signal Forms: Modelle und Datenbindung

--- book-rating/src/app/books/book-create-page/book-create-page.ts
+++ book-rating/src/app/books/book-create-page/book-create-page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Book } from '../shared/book';
+import { form, FormField } from '@angular/forms/signals';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-create-page',
-  imports: [],
+  imports: [FormField, JsonPipe],
   templateUrl: './book-create-page.html',
   styleUrl: './book-create-page.scss',
 })
 export class BookCreatePage {
+  #bookFormData = signal<Book>({
+    isbn: '',
+    title: '',
+    description: '',
+    rating: 1,
+    price: 0,
+    authors: []
+  });
+
+  protected readonly bookForm = form(this.#bookFormData);
 
 }
