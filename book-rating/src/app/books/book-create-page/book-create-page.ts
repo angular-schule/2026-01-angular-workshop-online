368ec8572a4ae888ac755db50bcb25d4530db2f7
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-26 13:49:18 +0100

    Signal Forms: Schema

--- book-rating/src/app/books/book-create-page/book-create-page.ts
+++ book-rating/src/app/books/book-create-page/book-create-page.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Book } from '../shared/book';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, max, maxLength, min, minLength, pattern, required, schema, validate, validateTree } from '@angular/forms/signals';
 import { JsonPipe } from '@angular/common';
 
 @Component({
@@ -19,6 +19,26 @@ export class BookCreatePage {
     authors: []
   });
 
-  protected readonly bookForm = form(this.#bookFormData);
+  protected readonly bookForm = form(this.#bookFormData, path => {
+    required(path.isbn, { message: 'ISBN is required.' });
+    minLength(path.isbn, 10, { message: 'ISBN must be min. 10 chars.' });
+    maxLength(path.isbn, 13, { message: 'ISBN must be max. 13 chars.' });
+    pattern(path.isbn, /^\d+$/, { message: 'ISBN must only contain numbers.' });
+
+    validate(path.isbn, ctx => {
+      if (!ctx.value().startsWith('978')) {
+        return { kind: 'isbnprefix', message: 'ISBN must start with 978.' };
+      }
+      return;
+    });
+
+    required(path.title, { message: 'Title is required.' });
+    maxLength(path.title, 80, { message: 'Title must not be longer than 80 chars.' });
+    required(path.price, { message: 'Price is required.' });
+
+    required(path.rating, { message: 'Rating is required' });
+    min(path.rating, 1, { message: 'Rating must in range of 1...5' });
+    max(path.rating, 5, { message: 'Rating must in range of 1...5' });
+  });
 
 }
