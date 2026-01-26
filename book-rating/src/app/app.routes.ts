0a6d5c77f066e92a9a081b61121584baeff6deda
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-26 09:54:17 +0100

    Routing: Links (und Kommentare)

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -6,5 +6,5 @@ export const routes: Routes = [
   // bei Weiterleitung vom leeren Pfad ist (fast) immer pathMatch:full nötig
   { path: '', redirectTo: 'books', pathMatch: 'full' },
   ...booksRoutes,
-  { path: '**', component: ErrorPage },
+  { path: '**', component: ErrorPage }, // Wildcard-Route
 ];
