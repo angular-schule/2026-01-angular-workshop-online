5c8b314dfa55452aabe4abe1be9b37d31abc36ee
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-28 11:27:33 +0100

    Detailseite mit `catchError()`

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -2,7 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookStore } from '../shared/book-store';
 import { Book } from '../shared/book';
-import { filter, map, switchMap } from 'rxjs';
+import { catchError, EMPTY, filter, map, switchMap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -18,6 +18,12 @@ export class BookDetailsPage {
   protected readonly book = toSignal(this.#route.paramMap.pipe(
     map(params => params.get('isbn')),
     filter(isbn => isbn !== null),
-    switchMap(isbn => this.#store.getSingle(isbn))
+    switchMap(isbn => this.#store.getSingle(isbn).pipe(
+      catchError(err => {
+        // Fehler ausgeben
+        console.log(err);
+        return EMPTY;
+      })
+    ))
   ));
 }
