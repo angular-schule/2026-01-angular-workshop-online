fe00c9d25fa7256a6e43ed5da3da7a9c86a31c1b
Ferdinand Malcher <ferdinand@malcher.media>
2026-01-28 11:16:13 +0100

    Detailseite mit `toSignal()`

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookStore } from '../shared/book-store';
 import { Book } from '../shared/book';
 import { filter, map, switchMap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book-details-page',
@@ -13,24 +14,10 @@ import { filter, map, switchMap } from 'rxjs';
 export class BookDetailsPage {
   #route = inject(ActivatedRoute);
   #store = inject(BookStore);
-  protected readonly book = signal<Book | undefined>(undefined);
 
-  constructor() {
-    this.#route.paramMap.pipe(
-      map(params => params.get('isbn')),
-      filter(isbn => isbn !== null),
-      switchMap(isbn => this.#store.getSingle(isbn))
-    ).subscribe(book => {
-      this.book.set(book);
-    });
-  }
-
+  protected readonly book = toSignal(this.#route.paramMap.pipe(
+    map(params => params.get('isbn')),
+    filter(isbn => isbn !== null),
+    switchMap(isbn => this.#store.getSingle(isbn))
+  ));
 }
-
-
-/*
-TODO Detailseite
-- BookStore HTTP: einzelnes Buch abrufen
-- Buch anzeigen
-
-*/
